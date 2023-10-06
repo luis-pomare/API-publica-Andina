6c70df724f71ae9ce58aa7c9bf0e8807a0e2c7c4
Luis Pomare <luispomare92@gmail.com>
2023-10-06 06:24:21 -0500

    Interpolar repuesta en estado del componente App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'API-publica-Andina';
+  repositorios: any[] = [];
 
   ngOnInit(): void {
-    async function llamarRepos() {
-      try {
-        const response = await fetch(
-          'https://api.github.com/search/repositories?q=stars:>0&sort=stars&order=desc&page=1&per_page=10'
-        );
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-        } else {
-          console.error('Error al obtener datos de la API');
-        }
-      } catch (error) {
-        console.error('Error en la solicitud:', error);
-      }
-    }
+    this.llamarRepos();
+  }
 
-    llamarRepos();
+  async llamarRepos() {
+    try {
+      const response = await fetch(
+        'https://api.github.com/search/repositories?q=stars:>0&sort=stars&order=desc&page=1&per_page=10'
+      );
+      if (response.ok) {
+        const data = await response.json();
+        this.repositorios = data.items;
+      } else {
+        console.error('Error al obtener datos de la API');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+    }
   }
 }
